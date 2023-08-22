9f90164f3cce20ae733c09c66a484b765a179ba8
Tuan Be <tuanblep0298@gmail.com>
2023-08-23 02:01:57 +0700

    update subscription screen

--- lib/screens/subscription/screens/subscription_screen2.dart
+++ lib/screens/subscription/screens/subscription_screen2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class SubscriptionScreen2 extends StatelessWidget {
   @override
